203a95755577017be306b366537aaab89a1d9eb0
rainboard01 <amdmsi4ever@hotmail.com>
2021-07-11 05:55:28 -0400

    Add unstyled color Slider

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ColorBox } from './ColorBox';
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 
 export const Palette = (props) => {
-    const colorBoxes = props.colors.map(color => (
-        <ColorBox background={ color.color } name={ color.name }/>
+    const [level, setLevel] = useState(500);
+
+    const colorBoxes = props.palette.colors[level].map(color => (
+        <ColorBox background={ color.hex } name={ color.name }/>
     ) );
     return (
         <div className="Palette">
             {/* Navbar goes here */}
-
+            <Slider 
+                defaultValue={ level }
+                min={ 100 }
+                max={ 900 }
+                onAfterChange={ setLevel }
+                step={ 100 }
+            />
             <div className="Palette-colors">
                 {/* bunch of color boxes*/}
                 { colorBoxes }
