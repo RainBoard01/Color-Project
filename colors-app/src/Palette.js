ea3febe2b168912ad1b3890d4603749b863e9f47
rainboard01 <amdmsi4ever@hotmail.com>
2021-07-11 03:04:57 -0400

    Added Palette Component

---
+++ colors-app/src/Palette.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Palette = () => {
+    return (
+        <div className="Palette">
+            {/* Navbar goes here */}
+            <div className="Palette-colors">
+                {/* bunch of color boxes*/}
+                
+            </div>
+            {/* footer eventually */}
+        </div>
+    )
+}
