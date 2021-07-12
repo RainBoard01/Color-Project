5f3a60b7343dfefd01a78d542bfcb48250ef9900
rainboard01 <amdmsi4ever@hotmail.com>
2021-07-12 12:53:50 -0400

    Add Snackbar and styled Select

--- colors-app/src/Navbar.js
+++ colors-app/src/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from "rc-slider";
-import Select from "@material-ui/core/Select";
-import { MenuItem } from '@material-ui/core';
+
+import { IconButton, MenuItem, Select, Snackbar } from '@material-ui/core';
+import CloseIcon from "@material-ui/icons/Close";
+
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export const Navbar = props => {
-    const { level, setLevel, format, setFormat } = props
+    const { level, setLevel, format, setFormat } = props;
+
+    const [open, setOpen] = useState(false);
+
+    const handleChange = event => {
+        setFormat(event.target.value);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <header className="navbar">
@@ -26,11 +39,28 @@ export const Navbar = props => {
                 </div>
             </div>
             <div className="select-container">
-                <Select value={ format } onChange={ e => setFormat(e.target.value) }>
-                    <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
+                <Select value={ format } onChange={ handleChange  }>
+                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
                     <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
                     <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem> 
                 </Select>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "left"
+                    }}
+                    open={open}
+                    autoHideDuration={3000}
+                    onClose={handleClose}
+                    message={ `Format Changed to ${ format.toUpperCase() }` }
+                    action={
+                        <>
+                            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                                <CloseIcon fontSize="small" />
+                            </IconButton>
+                        </>
+                    }
+                />
             </div>
         </header>
     )
