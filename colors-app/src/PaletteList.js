7c6506a9aafc54c6f890ce2308c8ca47e833965e
rainboard01 <amdmsi4ever@hotmail.com>
2021-07-13 10:12:27 -0400

    Start styling PaletteList

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import "./PaletteList.css";
+import { MiniPalette } from './MiniPalette';
 
 export const PaletteList = props => {
     const { palettes } = props;
     return (
-        <div>
-            <h1>React Colors</h1>
-            {palettes.map(palette => (
-                <p><Link to={ `/palette/${ palette.id }` }>{palette.paletteName}</Link></p>
-            ))}
+        <div className="paletteList">
+            <div className="container">
+                <nav className="nav">
+                    <h1>React Colors</h1>
+                </nav>
+                <div className="palettes">
+                    {palettes.map(palette => (
+                        <MiniPalette { ...palette } />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
