afa78df45de6e13e87eb2c1743b095dd8a566f02
rainboard01 <amdmsi4ever@hotmail.com>
2021-09-10 20:53:42 -0300

    Adjust text color based on luminance +minor changs

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import "./ColorBox.css";
+import chroma from 'chroma-js';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export const ColorBox = (props) => {
@@ -14,24 +15,27 @@ export const ColorBox = (props) => {
         return () => clearTimeout(timeout);
     },[copied]);
 
-    const { name, background, colorUrl,showLink } = props;
+    const { name, background, colorUrl, showLink } = props;
+    const isDarkColor = chroma(background).luminance() <= 0.1;
+    const isLightColor = chroma(background).luminance() >= 0.5;
+
     return (
         <CopyToClipboard text={ background } onCopy={ () => setCopied(true) }>
             <div style={{ background }} className="ColorBox">
                 <div style={{ background }} className={`copy-overlay ${ copied && "show" }`}/>
                 <div className={`copy-msg ${ copied && "show" }`}>
                     <h1>Copied!</h1>
-                    <p>{ background }</p>
+                    <p className={ isLightColor && "black-font" }>{ background }</p>
                 </div>
                 <div className="copy-container">
                     <div className="box-content">
-                        <span>{ name }</span>
+                        <span className={ isDarkColor && "white-font" }>{ name }</span>
                     </div>
-                    <button className="copy-button">Copy</button>
+                    <button className={ `copy-button ${ isLightColor && "black-font" }` }>Copy</button>
                 </div>
                 { showLink && (
                     <Link to={colorUrl} onClick={e => e.stopPropagation()}>
-                        <span className="see-more">MORE</span>
+                        <span className={ `see-more ${ isLightColor && "black-font" }` }>MORE</span>
                     </Link>
                 )}
             </div>
