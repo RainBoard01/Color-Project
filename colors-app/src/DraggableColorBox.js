821376179c6dea80b67683696a807722a2d6c049
rainboard01 <amdmsi4ever@hotmail.com>
2021-11-20 13:24:12 -0300

    Styled draggable colorbox

--- colors-app/src/DraggableColorBox.js
+++ colors-app/src/DraggableColorBox.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const Div = styled.div`
+const ColorBox = styled.div`
     background-color: ${ props => props.color };
     width: 20%;
     height: 25%;
@@ -12,7 +13,35 @@ const Div = styled.div`
     /*margin-bottom: -4.2px;*/
 `;
 
+const Text = styled.div`
+    display: flex;
+    height: 100%;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 10px;
+    color: rgba(0,0,0,0.5);
+`;
+
+const Name = styled.span`
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-size: 12px;
+`;
+
+const Delete = styled(DeleteIcon)`
+    font-size: 20px;
+    transition: transform .2s;
+    &:hover {
+        color: white;
+        transform: scale(1.3);
+    }
+`;
+
 export const DraggableColorBox = props => 
-    <Div color={props.color}>
-        { props.color }
-    </Div>;
+    <ColorBox color={props.color}>
+        <Text>
+            <Name>{ props.name }</Name>
+            <Delete />
+        </Text>
+    </ColorBox>;
